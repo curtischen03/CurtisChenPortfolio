2e98e59b61e20d3ca2ec5cd9dde9972b1408fe2b
Curtis Chen <curtischen1234@gmail.com>
2026-03-25 21:59:29 -0700

    Add in progress message

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import { Header } from "@/components/Header"
 import { LinksDock } from "@/components/LinksDock"
 import { Home } from "@/components/Home"
 import { Experience } from "@/components/Experience"
 import { Education } from "@/components/Education"
 import { Skills } from "@/components/Skills"
+import { motion } from "framer-motion"
+import { TypingAnimation } from "@/components/ui/typing-animation"
 
 export default function Page() {
   return (
@@ -14,7 +17,27 @@ export default function Page() {
       <Education />
       <Skills />
       <LinksDock />
-      <div className="min-h-[75dvh] bg-red-500"></div>
+      <div className="flex min-h-[75dvh] flex-row items-center justify-center">
+        <motion.div
+          initial={{ x: "-25vh", opacity: 0, scale: 0.5 }}
+          whileInView={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ type: "spring", stiffness: 50, damping: 20 }}
+          viewport={{ once: false }}
+        >
+          <h1 className="pb-8 text-center text-3xl font-bold sm:text-4xl">
+            <TypingAnimation
+              words={["In Progress..."]}
+              showCursor={true}
+              blinkCursor={true}
+              pauseDelay={2000}
+              cursorStyle="line"
+              auroraText={false}
+              typeSpeed={50}
+              loop
+            />
+          </h1>
+        </motion.div>
+      </div>
     </div>
   )
 }
